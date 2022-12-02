dc1884cbea77c8143715f329eb40d3e10ac22354
bawa-yatin <Yatin.bawa@mindbowser.com>
2022-12-02 17:07:25 +0530

    Completed UI and responsiveness of Story/Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,90 @@
 import React from "react";
 import "./Footer.css";
-import { Stack, Container, Row, Col } from "react-bootstrap";
+import {
+  Stack,
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  InputGroup,
+} from "react-bootstrap";
 import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import { BsEnvelope } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <Stack className="footer-bg">
-      <Container fluid>
-        <Row>
-          <Col xl={6} lg={6} md={6} sm={12} xs={12}>
-            <div className="left-content p-5">
-              <div className="left-heading fs-3">Get Sonar, Inc.</div>
-              <div className="social-icons mt-2">
-                <FaInstagram size={24} style={{ marginRight: "12px" }} />
-                <FaLinkedinIn size={24} />
-              </div>
+      <div className="content-container">
+        <Container fluid>
+          <Row>
+            <Col xl={6} lg={5} md={12} sm={12} xs={12}>
+              <div className="left-content p-5">
+                <div className="left-heading fs-3">Get Sonar, Inc.</div>
+                <div className="social-icons mt-2">
+                  <FaInstagram size={24} style={{ marginRight: "12px" }} />
+                  <FaLinkedinIn size={24} />
+                </div>
 
-              <ul className="list-items mt-5">
-                <li>About</li>
-                <li>FAQ</li>
-                <li>Contact</li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+                <ul className="list-items mt-5">
+                  <li>About</li>
+                  <li>FAQ</li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            </Col>
+
+            <Col xl={6} lg={7} md={12} sm={12} xs={12}>
+              <div className="right-content p-5">
+                <div className="right-heading my-4">
+                  Sign up to join our pod and get early access to Sonar.
+                </div>
+
+                <Row>
+                  <Form.Group
+                    as={Col}
+                    xl={8}
+                    lg={8}
+                    md={7}
+                    sm={7}
+                    xs={11}
+                    className="mx-auto"
+                  >
+                    <Form.Label className="input-label">Email</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text
+                        style={{
+                          backgroundColor: "#FFFFFF",
+                          borderRight: "unset",
+                        }}
+                      >
+                        <BsEnvelope size={22} style={{ color: "#949494" }} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        type="email"
+                        placeholder="pods@getsonar.org"
+                        className="email-field"
+                      />
+                    </InputGroup>
+                  </Form.Group>
+
+                  <Form.Group
+                    as={Col}
+                    xl={4}
+                    lg={4}
+                    md={12}
+                    sm={12}
+                    xs={12}
+                    className="btn-column mx-auto"
+                  >
+                    <Button className="form-button fs-5">Sign Up</Button>
+                  </Form.Group>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Stack>
   );
 };
---
+++ src/components/Story/allStory.js
@@ -0,0 +1,26 @@
+import Story1 from "../../assets/svg/story/story-1.svg";
+import Story2 from "../../assets/svg/story/story-2.svg";
+import Story3 from "../../assets/svg/story/story-3.svg";
+
+export const storyData = [
+  {
+    storyId: 1,
+    storyImg: Story1,
+    storyContent:
+      "Like many of you reading this, our team has experienced and witnessed the struggles of mental illness and have lost family members or friends as a result.",
+  },
+
+  {
+    storyId: 2,
+    storyImg: Story2,
+    storyContent:
+      "Gabe & Drew, bonded by a passion for improving mental healthcare and frustration about the lack of tools available to youth and their caregivers, started Sonar Mental Health out of Stanford University. Through hundreds of interviews and months of research, they identified that without more time or proper training, many of us find it incredibly difficult to identify when our loved ones require support.",
+  },
+
+  {
+    storyId: 3,
+    storyImg: Story3,
+    storyContent:
+      "With the foundation of years of experience leading mental healthcare and technology organizations and having united a team of clinicians, expert researchers, technologist, and youth leaders, Sonar aspires to address this problem. We are building a platform that proactively identifies when youth require support and improves the quality of care they receive.",
+  },
+];
